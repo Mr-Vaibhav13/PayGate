06ff3a0f16e9cb6cefa7988ec6887f7100ccb868
Vaibhav Sharma <vaibhav13mn@gmail.com>
2024-08-29 01:05:14 +0530

    first commit

---
+++ backend/security/encryption.js
@@ -0,0 +1,24 @@
+// encryption.js
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32); // Store this key securely
+const iv = crypto.randomBytes(16); // Store this IV securely
+
+const encrypt = (text) => {
+  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+};
+
+const decrypt = (text) => {
+  let textParts = text.split(':');
+  let iv = Buffer.from(textParts.shift(), 'hex');
+  let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+};
+
+module.exports = { encrypt, decrypt };
